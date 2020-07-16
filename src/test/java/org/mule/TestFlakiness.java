af0fc86323c880d2de34c27b99467307de4d9e8d
Fabian Gonzalez <fabian.gonzalez@gonzalezf-ltm3.internal.salesforce.com>
2020-07-16 11:16:43 -0300

    Test flakiness

---
+++ src/test/java/org/mule/TestFlakiness.java
@@ -0,0 +1,15 @@
+package org.mule;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestFlakiness {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
